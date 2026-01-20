b52b37cdbbfbe2ade1e1e2f05416ac2fa2af83ec
Ashwani-dev-code <ashwanipandey987654@gmail.com>
2026-01-20 21:04:57 +0530

    Added today classroom file

--- Lecture1/osmoduledate20jan.js
+++ Lecture1/osmoduledate20jan.js
@@ -7,9 +7,5 @@ const platform = os.platform();
 const uptime = os.uptime();
 
 const model=os.cpus()[0].model
-
-console.log("Total Memory (GB):", totalMemory.toFixed(2));
-console.log(model);
-console.log("Free Memory (GB):", freeMemory.toFixed(2));
-console.log("Platform:", platform);
-console.log("Uptime (seconds):", uptime);
+const systemlog=
+totalMemory
